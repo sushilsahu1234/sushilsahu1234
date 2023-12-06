299e9447d817f99c7e480e12b39630a778dfabf8
Sushil Kumar Sahu <146238718+sushilsahu1234@users.noreply.github.com>
2023-12-06 22:15:59 +0530

    Create up_counter.v

---
+++ up_counter.v
@@ -0,0 +1,12 @@
+module counter_1(clk,rst,count);
+  input clk,rst;
+  output reg [3:0]count;
+  always@(posedge clk)begin
+    if(rst)
+      count<=0;
+    else
+      count<= count+1;
+  end
+endmodule
+    
+  
